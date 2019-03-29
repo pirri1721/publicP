0d9c3aa0c87955697013e353ae3e216dcb6b970a
Silvestre Canora <canora.silvestre@gmail.com>
2019-03-29 13:43:34 +0100

    newAnims and weapons FBX // character controller with weapon root/unroot tested

---
+++ Parchis/Assets/Code/CharacterController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public GameObject weapon;
+
+    public Transform boneToAttach;
+    Transform weaponOriginalBone;
+    Vector3 weaponOriginalLocalposition;
+    Quaternion weaponOriginalRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        weaponOriginalBone = weapon.transform.parent;
+
+        weaponOriginalLocalposition = weapon.transform.localPosition;
+        weaponOriginalRotation = weapon.transform.localRotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void RootWeapon()
+    {
+        weapon.transform.SetParent(weaponOriginalBone);
+        weapon.transform.localPosition = weaponOriginalLocalposition;
+        weapon.transform.localRotation = weaponOriginalRotation;
+    }
+
+    public void UnRootWeapon()
+    {
+        weapon.transform.SetParent(boneToAttach);
+    }
+}
